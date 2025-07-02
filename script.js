07a2362ae9f0b0c71104b691f1330195e1557f2d
FIXED-TERM Gyarmati Tamas Jozsef (MA-WS/PAS-EDW) <gyt6bp@bosch.com>
2025-07-02 13:11:42 +0200

    Inputs ready

---
+++ script.js
@@ -0,0 +1,23 @@
+/* API */
+const apiUrl = "http://localhost:5290";
+const employeePostUrl = "http://localhost:5290/api/employees/json";
+
+/* Buttons */
+const addBtn = document.getElementById("addButton");
+const editBtn = document.getElementById("editButton");
+const deleteBtn = document.getElementById("deleteButton");
+
+addBtn.addEventListener("click", () => {
+    /*    
+    Tároljuk el a mezőket
+    Szedjük ki az adatokat belőle
+    Hívjuk meg az API hívást (POST)
+    Adjuk át Fetch-ben az adatokat
+
+    Hozzuk létre a megfelelő táblázati elemeket
+    Az értéküket pedig a kiszedett értékekre cseréljük ki
+    Gombokat ne felejtsük el odatenni
+    */
+
+
+});
